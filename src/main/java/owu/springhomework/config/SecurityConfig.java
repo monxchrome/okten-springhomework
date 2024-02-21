3e639ec1f380e8c77b926174cf6e98beb33cb06f
monxchrome <inacheat@gmail.com>
2024-02-21 16:53:06 +0100

    homework 6-1

--- src/main/java/owu/springhomework/config/SecurityConfig.java
+++ src/main/java/owu/springhomework/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -77,9 +78,11 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**", "/error").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
-                .exceptionHandling(configurer -> configurer.authenticationEntryPoint(authHandler))
+                .oauth2ResourceServer(oAuth2ResourceServerConfigurer ->
+                        oAuth2ResourceServerConfigurer.jwt(Customizer.withDefaults()))
+//                .authenticationProvider(authenticationProvider)
+//                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+//                .exceptionHandling(configurer -> configurer.authenticationEntryPoint(authHandler))
                 .build();
     }
 
